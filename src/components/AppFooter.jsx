71b7b66833f311b0f6278bcce25045bd3079501e
giustoantonadev <giustoantonadev@gmail.com>
2026-03-20 19:06:05 +0100

    footer menu object appfooter component

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,5 +1,20 @@
-export default function AppFooter(){
-    return(
-        <p> test</p>
+import { Link } from "react-router-dom"
+import footerMenu from "../data/footerMenu"
+
+export default function AppFooter() {
+    return (
+        <footer className="bg-dark text-white py-4 mt-5">
+            <div className="container">
+                {footerMenu.map(item => (
+                    <Link
+                        key={item.id}
+                        to={item.link}
+                        className="mx-2 text-white text-decoration-none"
+                    >
+                        {item.text}
+                    </Link>
+                ))}
+            </div>
+        </footer>
     )
 }
---
+++ src/data/footerMenu.js
@@ -0,0 +1,45 @@
+const footerMenu = [
+    {
+        id: 1,
+        link:'/about',
+        text:'Chi siamo'
+    },
+    {
+        id: 2,
+        link:'/contact',
+        text:'Contatti'
+    },
+    {
+        id: 3,
+        link:'/faq',
+        text:'FAQ'
+    },
+    {
+        id: 4,
+        link:'/shipping',
+        text:'Spedizioni'
+    },
+    {
+        id: 5,
+        link:'/returns',
+        text:'Resi'
+    },
+    {
+        id: 6,
+        link:'/privacy',
+        text:'Privacy Policy'
+    },
+    {
+        id: 7,
+        link:'/terms',
+        text:'Termini e condizioni'
+    },
+    {
+        id: 8,
+        link:'/support',
+        text:'Supporto'
+    }
+]
+
+
+export default footerMenu
