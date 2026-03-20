ac053601042c81f2bc2cd1d9cbdac24e2546af01
giustoantonadev <giustoantonadev@gmail.com>
2026-03-20 12:29:34 +0100

    Install route create home, products, whoarewe pages components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Products from "./pages/Products"
+import WhoAreWe from "./pages/WhoAreWe"
 
 
 
 function App() {
- 
+
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
