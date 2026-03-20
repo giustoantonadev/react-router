937fe9820f09b456d6ed2b8044f07c1640ab9431
giustoantonadev <giustoantonadev@gmail.com>
2026-03-20 18:24:16 +0100

    Product page through fetch API

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,38 @@
-export default function Products (){
+import { useState, useEffect } from "react"
+
+export default function Products() {
+
+    const api_url = 'https://fakestoreapi.com/products'
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch(api_url)
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data)
+            })
+    }, [])
+
     return (
-        <p>prodotti provvisorio</p>
+        <div className="container">
+            <div className="row g-4 mt-2">
+                {products.map(product => (
+                    <div className="col-12 col-md-6 col-lg-4 col-xl-3" key={product.id}>
+                        <div className="card" style={{ width: "20rem" }}>
+                            <img src={product.image} className="card-img-top" style={{ objectFit: "contain" }} alt="img" />
+                            <div className="card-body">
+                                <h5 className="card-title">{product.title}</h5>
+                                <p className="card-text">${product.price}</p>
+                                {<p className="card-text">{product.description}</p>}
+                            </div>
+                            <div className="card-footer">{product.category}</div>
+                        </div>
+
+                    </div>
+
+                ))}
+            </div>
+        </div>
     )
 }
